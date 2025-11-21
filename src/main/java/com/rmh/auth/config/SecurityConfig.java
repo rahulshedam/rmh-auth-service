263002a1af6fe2f45954331220b92461f9155824
Rajrahul-talentica <rahul.raj1@talentica.com>
2025-11-21 13:47:01 +0530

    NEX-1283: Added Error Response and Status Codes

--- src/main/java/com/rmh/auth/config/SecurityConfig.java
+++ src/main/java/com/rmh/auth/config/SecurityConfig.java
@@ -22,9 +22,7 @@ public class SecurityConfig {
     private static final String[] SWAGGER_WHITELIST = {
             "/swagger-ui/**",
             "/swagger-ui.html",
-            "/swagger-ui/index.html",
-            "/v3/api-docs/**",
-            "/v3/api-docs/swagger-config"
+            "/api-docs/**"
     };
 
     public SecurityConfig(
